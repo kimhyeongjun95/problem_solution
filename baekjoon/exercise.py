360a45bf095eb93252fccb7bc6de898333fb7500
hyeongjunkim <hjthelegend@naver.com>
2021-09-01 23:56:57 +0900

    ps push

--- baekjoon/exercise.py
+++ baekjoon/exercise.py
@@ -1,13 +1,13 @@
-n = int(input())
+# 같은 색상이 상하좌우 인접 -> 같은 구역
+# 적록색약 R, G 같은 색상으로 보임
 
-for _ in range(n):
-    scores = list(map(int, input().split()))
+# RRRBB 00011
+# GGBBB 00111
+# BBBRR 11100
+# BBRRR 11000
+# RRRRR 00000
+# 적록색약은 3개로 보이고 아닌사람은 4개로 보임. (구역)
+# dfs or bfs
 
-    average = sum(scores[1:]) / scores[0]
-
-    count = 0
-    for i in scores[1:]:
-        if i > average:
-            count += 1
-    
-    print(f'{(count/scores[0])*100:.3f}%')
+import sys
+input = sys.stdin.readline
