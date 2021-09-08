041386294d3d3a3c5807e7c3cad104ec1a09cfaa
hyeongjunkim <hjthelegend@naver.com>
2021-09-08 23:45:20 +0900

    ps push

---
+++ baekjoon/16236.py
@@ -0,0 +1,15 @@
+# N x N 2차원 행렬
+# M : 물고기
+# 아기 상어 : 1 마리
+# 한 칸에는 물고기가 최대 1마리 / 아기 상어의 크기는 처음에 2
+
+# if 아기상어 > 물고기 : eat
+# elif 아기상어 == 물고기 : pass
+# elif 아기상어 < 물고기 : not pass
+
+# if eatable == 1: go
+# elif eatable > 1: go nearest
+#     when moving / pass min
+#     여러개라면 위 -> 왼쪽 ???
+# elif eatable == 0: call mom shark
+# 어떻게 풀면 좋을까??
