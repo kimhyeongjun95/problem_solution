1c980eaecdc358456f22301879289c4e6d1bd0f6
hyeongjunkim <hjthelegend@naver.com>
2021-12-02 23:39:57 +0900

    ps push

--- programmers/큰 수 만들기.py
+++ programmers/큰 수 만들기.py
@@ -9,9 +9,11 @@ def solution(number, k):
             stack.pop()
         stack.append(i)
     
+    return ''.join(stack[:len(stack)-k])
     return ''.join(stack)
+    # 히든 테스트 케이스 : k가 남은 경우
+    # '54321'
 
-    
 
 
 print(solution('1924', 2)) # '94'
