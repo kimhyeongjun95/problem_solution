f9a0884f1badeeb77c5a894b8e4ada4bf065c07f
KimHyeongJun <hjthelegend@naver.com>
2022-01-11 23:55:23 +0900

    ps push

--- programmers/이중우선순위 큐.py
+++ programmers/이중우선순위 큐.py
@@ -1,19 +1,4 @@
 # 프로그래머스 이중우선순위 큐
-# import heapq
-# def solution(operations):
-#     answer = []
-#     heap = []
-#     for operation in operations:
-#         c, n = operation.split(' ')
-#         if c == 'I':
-#             heapq.heappush(heap, int(n))
-#         else:
-#             if heap:
-#                 if n == '1': # 최대값
-#                     heap.pop(heap.index(heapq.nlargest(1, heap)))
-#                 else: # 최소값
-#                     heapq.heappop(heap)
-
 def solution(operations):
     answer = []
     for operation in operations:
@@ -27,6 +12,7 @@ def solution(operations):
                 else:
                     answer.pop(0)
         answer.sort()
+        
     if not answer:
         return [0,0]
     else:
@@ -34,4 +20,3 @@ def solution(operations):
 
 print(solution(["I 16","D 1"])) # [0, 0]
 print(solution(["I 7","I 5","I -5","D -1"])) # [7, 5]
-
