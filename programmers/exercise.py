89d56d5dd48fa5e872da43ddcfd174ebb023a6a6
KimHyeongJun <hjthelegend@naver.com>
2022-03-28 23:41:31 +0900

    ps push

--- programmers/exercise.py
+++ programmers/exercise.py
@@ -1,38 +1,14 @@
-# 프로그래머스 더 맵게
+# 프로그래머스 정수 삼각형
 
-# 모든 음식의 스코빌 지수를 K 이상
-# 지수가 가장 낮은 두 개의 음식 => 섞어서 새로운 음식
+def solution(triangle):
+    answer = 0
+    return answer
 
-# mix = min 스코빌 + (2nd min 스코빌 * 2)
+print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
+# 30
 
-# 모든 음식의 스코빌 지수가 K 이상 될때까지 반복
-# 섞어야 하는 최소 횟수 return
-
-import heapq
-
-def solution(scoville, K):
-
-    heapq.heapify(scoville)
-    count = 0
-
-    while True:
-        
-        first = heapq.heappop(scoville)
-        second = heapq.heappop(scoville)
-        temp = first + (second * 2)
-
-        heapq.heappush(scoville, temp)
-        count += 1
-
-        for i in scoville:
-            if i < K:
-                break
-        else:
-            return count
-
-        if len(scoville) == 1:
-            return -1
-
-
-print(solution([1, 2, 3, 9, 10, 12], 7))
-# 2
+#     [7]
+#    [3, 8]
+#   [8, 1, 0]
+#  [2, 7, 4, 4]
+# [4, 5, 2, 6, 5]
