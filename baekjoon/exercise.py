c8d823116b9969ae78bc45027dd76d3ff354139e
KimHyeongJun <hjthelegend@naver.com>
2022-04-21 00:10:02 +0900

    ps push

--- baekjoon/exercise.py
+++ baekjoon/exercise.py
@@ -1,38 +1,45 @@
-# 백준 3584 가장 가까운 공통 조상
+# 백준 4485 녹색 옷 입은 애가 젤다지?
 
-# 루트 트리
-# 두 정점이 주어질 때, 
-# 가장 가까운 공통 조상 : 두 노드를 모두 자손으로 가지면서 깊이가 가장 깊은(가까운) 노드
+# 화폐의 단위 : 루피
+# 도둑루피 : 소지한 루피 감소
 
-# ex) 15, 11 => 4와 8중 4
+# N x N 동굴
+# [0][0]번 칸
+# [N-1][N-1]까지 이동
+# 잃는 금액을 최소로 하여 이동
+# 잃는 최소 금액 return
 
-import sys
-input = sys.stdin.readline
+from collections import deque
+import copy
+tc = 1
+dxy = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+# dxy = [(0, 1), (1, 0)]
+def bfs(queue, arr, visited):
+    while queue:
+        x, y = queue.popleft()
+        print(x, y)
+        print(visited)
+        for dx, dy in dxy:
+            nx = x + dx
+            ny = y + dy
+            if -1 < nx < n and -1 < ny < n:
+                if visited[nx][ny] > visited[x][y] + arr[nx][ny]:
+                    visited[nx][ny] = visited[x][y] + arr[nx][ny]
+                    queue.append((nx, ny))
+    return visited[n-1][n-1]
 
-t = int(input())
-for _ in range(t):
+while True:
     n = int(input())
-    trees = [[] for _ in range(n+1)]
-    for _ in range(n-1):
-        a, b = map(int, input().split())
-        trees[b].append(a)
-    # 16, 7
-    one, two = map(int, input().split())
-    one_tree = [one]
-    two_tree = [two]
-    while trees[one]:
-        one_tree.append(trees[one][0])
-        one = trees[one][0]
-    while trees[two]:
-        two_tree.append(trees[two][0])
-        two = trees[two][0]
+    if n == 0:
+        break
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    visited = [[float('inf')] * n for _ in range(n)]
+    visited[0][0] = arr[0][0]
+    answer = float('inf')
+    queue = deque([(0, 0)])
+    answer = bfs(queue, arr, visited)
+    for i in visited:
+        print(i)
+    print(f'Problem {tc}: {answer}')
 
-    flag = False
-    for i in one_tree:
-        if flag:
-            break
-        for j in two_tree:
-            if i == j:
-                print(i)
-                flag = True
-                break
+    tc += 1
