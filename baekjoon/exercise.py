a78a043869ad812dff36f5d7fca47c074a4384b9
kimhyeongjun95 <hjthelegend@naver.com>
2022-06-01 23:53:50 +0900

    ps push

--- baekjoon/exercise.py
+++ baekjoon/exercise.py
@@ -1,96 +1,56 @@
-# 백준 1038 감소하는 수
+# 백준 2504 괄호의 값
 
-# K개의 글자를 가르칠 시간
-# 학생들은 K개의 글자로만 이루어진 단어만 read O
-# 어떤 K개의 글자를 가르쳐야 학생들이 읽을 수 있는 단어가 최대가 되는지?
+# 올바른 괄호란
+# 1. () and []
+# 2. X가 올바른 괄호열 => (X) [X] 올바른 괄호열
+# 3. X and Y 모두 올바른 괄호열 => XY 올바른 괄호열
 
-# 모든 단어는 
-# starts with "anta"
-# ends with "tica"
-# 남극 언어는 N개만 존재
+# 괄호값
+# () 2
+# [] 3
+# (X) => 2 x 값(X)
+# [X] => 3 x 값(X)
+# XY => 값(XY) = 값(X) + 값(Y)
 
-# antatica로 5개를 가져감.
-# K가 5개 미만이라면 0 return
+# 괄호값 return
+# 올바르지 못한 괄호열 => 0 return
 
-# from collections import defaultdict
-# from itertools import combinations
-# import sys
-# input = sys.stdin.readline
-# n, k = map(int, input().split())
-# words = []
-# for _ in range(n):
-#     words.append(input().strip())
-# print(words)
-# def find():
-#     if k < 5:
-#         return 0
-#     if k >= 26:
-#         return n
-    
-#     result = 0
-#     check = defaultdict(int)
-#     for word in words:
-#         for i in range(4, len(word)-4):
-#             check[word[i]] = 1
-#     checked_set = []
-#     for word in words:
-#         val = set("antatica")
-#         for i in word:
-#             val.add(i)
-#         checked_set.append(val)
-#     for combo in combinations(check, k-5):
-#         isin = set('antatica')
-#         temp = ''.join(combo)
-#         for i in temp:
-#             isin.add(i)
-#         count = 0
-#         for check_set in checked_set:
-#             if check_set.issubset(isin):
-#                 count += 1
-#         result = max(result, count)
-#     return result
-    
-# answer = find()
-# print(answer)
+# 1. stack에 ) or ]이면 [-1]이 ( or [
+# 1-1. (나 [면 들어와도 된다.
+# 2. (( [[ 겹치면 mul에 append
+# 2-1. mul[-1]과 i가 매치한다면 pop의 type에 따라 *=
+# 2-2. val에 더하기 
 
-import sys
+def caculate():
+    stack = []
+    answer = 0
+    count = 1
+    for i in range(len(s)):
+        if s[i] == '(':
+            count *= 2
+            stack.append(s[i])
+        if s[i] == '[':
+            count *= 3
+            stack.append(s[i])
+        if s[i] == ')':
+            if not stack or stack[-1] == '[':
+                return 0
+            if s[i-1] == '(':
+                answer += count
+                stack.pop()
+                count //= 2
+        if s[i] == ']':
+            if not stack or stack[-1] == '(':
+                return 0
+            if s[i-1] == '[':
+                answer += count
+                stack.pop()
+                count //= 3
+        print(stack)
+        print(answer, count, s[i])
+        print(i, len(s))
+    return answer
 
-n, k = map(int, input().split())
-
-if k < 5:
-    print(0)
-    exit()
-elif k == 26:
-    print(n)
-    exit()
-
-answer = 0
-words = [set(sys.stdin.readline().rstrip()) for _ in range(n)]
-learn = [0] * 26
-
-for c in ('a', 'c', 'i', 'n', 't'):
-    learn[ord(c) - ord('a')] = 1
-
-def dfs(idx, cnt):
-    global answer
-    if cnt == k - 5:
-        see = 0
-        for word in words:
-            check = True
-            for w in word:
-                if not learn[ord(w) - ord('a')]:
-                    check = False
-                    break
-            if check:
-                see += 1
-        answer = max(answer, see)
-        return
-    
-    for i in range(idx, 26):
-        if not learn[i]:
-            learn[i] = True
-            dfs(i, cnt+1)
-            learn[i] = False
-
-dfs(0, 0)
+s = input()
+answer = caculate()
 print(answer)
