69f1a413d94043e043319a3f3a975eca7f78f327
kimhyeongjun95 <hjthelegend@naver.com>
2022-11-18 00:17:16 +0900

    ps push

--- programmers/exercise.js
+++ programmers/exercise.js
@@ -1,31 +1,25 @@
-// 프로그래머스 땅따먹기
-// N행 4열
+// 프로그래머스 타겟 넘버
 
-// 1행부터 밟으면서 내려올때, 같은 열을 연속해서 밟을 수 없음
-// 최대 얻을 수 있는 점수 return
+// n개의 음이 아닌 정수
+// 순서를 바꾸지 않고 적절히 더하거나 빼서 타겟 넘버
+// 타겟 넘버를 만드는 방법의 수 return
 
-function solution(land) {
-    const N = land.length;
-    dp = Array.from(Array(N), () => Array(4).fill(0));
-    for (let i = 0; i < 4; i++) dp[0][i] += land[0][i];
-    for (let i = 1; i < N; i++) {
-        for (let j = 0; j < 4; j++) {
-            let maxNum = 0;
-            for (let k = 0; k < 4; k++) {
-                if (j == k) continue;
-                maxNum = Math.max(maxNum, dp[i - 1][k]);
-            }
-            dp[i][j] = land[i][j] + maxNum;
+function solution(numbers, target) {
+    let answer = 0;
+    const N = numbers.length;
+    const find = (idx, now) => {
+        if (idx === N) {
+            if (now === target) answer += 1;
+            return;
         }
-    }
-    return Math.max(...dp[N - 1]);
+        find(idx + 1, now + numbers[idx]);
+        find(idx + 1, now - numbers[idx]);
+    };
+    find(0, 0);
+    return answer;
 }
 
-console.log(
-    solution([
-        [1, 2, 3, 5],
-        [5, 6, 7, 8],
-        [4, 3, 2, 1],
-    ])
-);
-// 16
+console.log(solution([1, 1, 1, 1, 1], 3));
+// 5
+console.log(solution([4, 1, 2, 1], 4));
+// 2
--- programmers/타겟 넘버.js
+++ programmers/타겟 넘버.js
@@ -1,30 +1,25 @@
 // 프로그래머스 타겟 넘버
 
+// n개의 음이 아닌 정수
+// 순서를 바꾸지 않고 적절히 더하거나 빼서 타겟 넘버
+// 타겟 넘버를 만드는 방법의 수 return
+
 function solution(numbers, target) {
     let answer = 0;
-    const find = (count, sum) => {
-
-        if (count === numbers.length) {
-            if (sum === target) {
-                answer += 1;
-            }
+    const N = numbers.length;
+    const find = (idx, now) => {
+        if (idx === N) {
+            if (now === target) answer += 1;
             return;
         }
-        
-        find(count+1, sum + numbers[count]);
-        find(count+1, sum - numbers[count]);
-    }
-    find(0, 0)
+        find(idx + 1, now + numbers[idx]);
+        find(idx + 1, now - numbers[idx]);
+    };
+    find(0, 0);
     return answer;
 }
 
-// console.log(solution(
-// 	[1, 1, 1, 1, 1],
-// 	3
-// ))
+console.log(solution([1, 1, 1, 1, 1], 3));
 // 5
-console.log(solution(
-	[4, 1, 2, 1],
-	4,
-))
+console.log(solution([4, 1, 2, 1], 4));
 // 2
