ec74865be2bed7d600d6368f35c20387157c38d7
kimhyeongjun95 <hjthelegend@naver.com>
2022-12-02 01:01:10 +0900

    ps push

--- leetcode/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-eckPalindrome = (sub) => {
-        for (let k = 0; k < parseInt(sub.length / 2); k++) {
-            if (sub[k] !== sub[sub.length - k]) return false;
-        }
-        return true;
-    };
