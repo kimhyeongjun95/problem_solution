f7adb0d140d68ff78259f2671dc4c4f256c247b4
kimhyeongjun95 <hjthelegend@naver.com>
2022-12-02 11:56:53 +0900

    ps push

---
+++ leetcode/findMin.js
@@ -0,0 +1,19 @@
+// 길이 n의 오름차순 정렬 배열
+// rotated between 1 ~ n times
+// 오른쪽 한 칸 으로 넘어간다.
+
+// 배열에서 가장 작은 수를 찾아야 한다.
+// (log n)이하로 해결
+
+// 이분탐색?
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {};
+
+console.log(findMin([3, 4, 5, 1, 2]));
+// 1
+console.log(findMin([4, 5, 6, 7, 0, 1, 2]));
+// 0
