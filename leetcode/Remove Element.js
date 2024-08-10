ce4bdd00d015d43d421494a225c93486e33a9ae8
kimhyeongjun95 <hjthelegend@naver.com>
2024-08-10 17:10:47 +0900

    ps push

---
+++ leetcode/Remove Element.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  let k = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k += 1;
+    }
+  }
+
+  return k;
+};
+
+console.log(removeElement([3, 2, 2, 3], 3));
+// 2
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
+// 5
